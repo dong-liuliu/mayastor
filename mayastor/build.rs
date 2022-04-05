9b842af10c109ae8cdafd14744ddc04b92e6bc3e
Liu Xiaodong <xiaodong.liu@intel.com>
2022-04-05 02:14:53 +0000

    build: correct SPDK lib compilation
    Signed-off-by: Liu Xiaodong <xiaodong.liu@intel.com>

--- mayastor/build.rs
+++ mayastor/build.rs
@@ -4,4 +4,7 @@ fn main() {
     let spdk_rpath = env::var("DEP_SPDK_BUNDLE_ROOT").unwrap();
     println!("cargo:rustc-link-search=native={}", spdk_rpath);
     println!("cargo:rustc-link-arg=-Wl,-rpath={}", spdk_rpath);
+    println!("cargo:rustc-link-lib=dylib=spdk-bundle");
+    let spdk_bundle_libs ="-luring -luuid -lIPSec_MB -laio -lcrypto -ldl -lm -lnuma -lrt";
+    println!("cargo:rustc-flags={}", spdk_bundle_libs);
 }
